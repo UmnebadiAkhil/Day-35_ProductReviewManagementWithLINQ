356c571f5686a7c73206342ffb3ddc74609fe2a3
Akhil <akhilu5042@gmail.com>
2022-02-13 10:27:59 +0530

    Welcome to product review management problem statement

---
+++ ProductReviewManagementWithLINQ/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Console.WriteLine("Hello Welocome to Product Review management problem statement!");
+        }
+    }
+
+}
