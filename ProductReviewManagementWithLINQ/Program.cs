0ecd592b6a3abfdfa4d9b3236df547f41b3bfcab
Akhil <akhilu5042@gmail.com>
2022-02-13 10:35:59 +0530

    UC1_Producct Review management

--- ProductReviewManagementWithLINQ/Program.cs
+++ ProductReviewManagementWithLINQ/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProductReviewManagementWithLINQ;
+using System;
 
 namespace ProductReviewManagement
 {
@@ -7,6 +8,31 @@ namespace ProductReviewManagement
         static void Main(string[] agrs)
         {
             Console.WriteLine("Hello Welocome to Product Review management problem statement!");
+
+
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 2, UserID = 1, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 3, UserID = 2, Rating = 5, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 4, UserID = 2, Rating = 4, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 5, UserID = 3, Rating = 2, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 6, UserID = 4, Rating = 1, Review = "Bad", isLike = true },
+                new ProductReview() { ProductID = 1, UserID = 3, Rating = 1.5, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 11, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 10, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 12, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 13, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 14, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 15, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 16, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+            };
+
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine("ProductID: " + list.ProductID + "UserId: " + list.UserID + "Rating: " + list.Rating
+                    + "Review: " + list.Review + "IsLike: " + list.isLike);
+            }
         }
     }
 
