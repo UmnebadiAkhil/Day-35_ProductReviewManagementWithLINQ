6b98fb147da8caead8e7d88f5d6f93fad5547cb6
Akhil <akhilu5042@gmail.com>
2022-02-13 12:17:00 +0530

    UC9_RetrieveAllRecords

--- ProductReviewManagementWithLINQ/Program.cs
+++ ProductReviewManagementWithLINQ/Program.cs
@@ -12,20 +12,32 @@ namespace ProductReviewManagement
 
             List<ProductReview> productReviewList = new List<ProductReview>()
             {
-                new ProductReview() { ProductID = 1, UserID = 1, Rating = 5, Review = "Good", isLike = true },
+
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 2, Review = "Good", isLike = true },
                 new ProductReview() { ProductID = 2, UserID = 1, Rating = 4, Review = "Good", isLike = true },
                 new ProductReview() { ProductID = 3, UserID = 2, Rating = 5, Review = "Good", isLike = true },
-                new ProductReview() { ProductID = 4, UserID = 2, Rating = 4, Review = "Good", isLike = true },
-                new ProductReview() { ProductID = 5, UserID = 3, Rating = 2, Review = "nice", isLike = false },
-                new ProductReview() { ProductID = 6, UserID = 4, Rating = 1, Review = "Bad", isLike = true },
-                new ProductReview() { ProductID = 1, UserID = 3, Rating = 1.5, Review = "nice", isLike = false },
-                new ProductReview() { ProductID = 11, UserID = 10, Rating = 4, Review = "nice", isLike = true },
-                new ProductReview() { ProductID = 10, UserID = 10, Rating = 4, Review = "nice", isLike = true },
-                new ProductReview() { ProductID = 12, UserID = 10, Rating = 4, Review = "nice", isLike = true },
-                new ProductReview() { ProductID = 13, UserID = 10, Rating = 4, Review = "nice", isLike = true },
-                new ProductReview() { ProductID = 14, UserID = 10, Rating = 4, Review = "nice", isLike = true },
-                new ProductReview() { ProductID = 15, UserID = 10, Rating = 4, Review = "nice", isLike = true },
-                new ProductReview() { ProductID = 16, UserID = 10, Rating = 4, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 4, UserID = 3, Rating = 6, Review = "Good", isLike = false },
+                new ProductReview() { ProductID = 5, UserID = 1, Rating = 2, Review = "okay", isLike = true },
+                new ProductReview() { ProductID = 6, UserID = 6, Rating = 1, Review = "bad", isLike = true },
+                new ProductReview() { ProductID = 7, UserID = 4, Rating = 1, Review = "Good", isLike = false },
+                new ProductReview() { ProductID = 8, UserID = 8, Rating = 9, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 2, UserID = 1, Rating = 10, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 10, UserID = 2, Rating = 8, Review = "Excellent", isLike = true },
+                new ProductReview() { ProductID = 11, UserID = 4, Rating = 3, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 8, UserID = 3, Rating = 5, Review = "nice", isLike = true },
+                new ProductReview() { ProductID = 9, UserID = 4, Rating = 2, Review = "bad", isLike = false },
+                new ProductReview() { ProductID = 2, UserID = 5, Rating = 7, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 4, UserID = 8, Rating = 3, Review = "very bad", isLike = true },
+                new ProductReview() { ProductID = 11, UserID = 3, Rating = 6, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 7, UserID = 5, Rating = 8, Review = "Good", isLike = true },
+                new ProductReview() { ProductID = 6, UserID = 1, Rating = 7, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 4, UserID = 3, Rating = 7, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 9, UserID = 2, Rating = 4, Review = "bad", isLike = true },
+                new ProductReview() { ProductID = 3, UserID = 7, Rating = 6, Review = "okay", isLike = true },
+                new ProductReview() { ProductID = 1, UserID = 1, Rating = 8, Review = "nice", isLike = false },
+                new ProductReview() { ProductID = 8, UserID = 1, Rating = 9, Review = "Good", isLike = false },
+                new ProductReview() { ProductID = 5, UserID = 1, Rating = 4, Review = "bad", isLike = true },
+                new ProductReview() { ProductID = 10, UserID = 1, Rating = 7, Review = "nice", isLike = true },
             };
 
             //foreach (var list in productReviewList)
@@ -61,7 +73,11 @@ namespace ProductReviewManagement
 
             //UC 8
             //Created data table
-            ProductReviewDataTable.AddDataIntoDataTable();
+            //ProductReviewDataTable.AddDataIntoDataTable();
+
+            //UC 9
+            //Retrieve all records from data table from islike
+            ProductReviewDataTable.RetrieveAllRecordsWhoseIsLikeIsTrue();
         }
     }
 
